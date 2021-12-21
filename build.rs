962680af14cd56999127eea4c72eba480febe60f
Amir Tuval <amirtu@codota.com>
2021-12-21 09:59:03 +0200

    fix cross compiling for apple

--- build.rs
+++ build.rs
@@ -1,12 +1,8 @@
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-extern crate cc;
-
-#[cfg(any(target_os = "macos", target_os = "ios"))]
 fn main() {
-    println!("cargo:rustc-link-lib=framework=IOKit");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rustc-link-lib=framework=Foundation");
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS");
+    if target_os == Ok("macos".to_string()) || target_os == Ok("ios".to_string()) {
+        println!("cargo:rustc-link-lib=framework=IOKit");
+        println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        println!("cargo:rustc-link-lib=framework=Foundation");
+    }
 }
-
-#[cfg(not(any(target_os = "macos", target_os = "ios")))]
-fn main() {}
